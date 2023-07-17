c3771019d56b2d6b154ec31afd685f9ad48bec6e
Sarvesh <138293616+thanosuserss@users.noreply.github.com>
2023-07-17 14:03:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Development(Config):
     # the name to display in your alive message
     ALIVE_NAME = "rishabh"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
-    DB_URI = "Your value"
+    DB_URI = "postgres://agnnsppd:NOC_2Ax0Ha96AS1UoyHLw2HS-ioB5toh@satao.db.elephantsql.com/agnnsppd"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
     STRING_SESSION = "1BVtsOIEBu7-pxyTR-itmeAzW_7A0y6TJzws3aC6utpOjLtElI5WNDO1PXt5ZG3IV0a2TbZEqibIuxWQHsh7rOV-JPwsPlVHHTR2hrLYR9WD8vkDxYM1QctELwMJ3Fx14BXW1SI3vQtVbNsunEdf9dUMg1lAMzqyBqcbxo0EGaUQ6qpMPPJReTaZ7QLlAWbJ201lKjklAxFGYEC63j05XDAPXtacoYxaaPVjbg0aPhEh-MumHevhUR_A8tw4HfGusLR-0OrZsaj9-Iv_AepIK4sF8pjqk9iVKDPKWciRT9CjIIgbtBscVVqhtKCjItEO6UMu7W37GAoQBtbn_65ai6OdPzAKllvA="
     # create a new bot in @botfather and fill the following vales with bottoken
